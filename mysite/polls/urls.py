2e90042422889a4a296f58bce8d52df61803d773
MattHew Phraxayavong <mphraxa@gmail.com>
2019-06-06 07:50:10 -0700

    initial server setup complete, timezone settings changed to reflect Los Angeles instead of PST

---
+++ mysite/polls/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from django.contrib import admin
+from django.urls import include, path
+
+from . import views
+
+urlpatterns = [
+
+    path('', views.index, name='index')
+
+    ]
+
+# urlpatterns = [
+#     path('', views.index, name='index'),
+# ]
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/matthewphraxayavong/dev/DjangoTutorial/venv/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
